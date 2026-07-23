35cc836e382b22dfd2cc80aec40b44226b520f55
Nabhye <nabhyetripathy15@gmail.com>
2026-07-23 20:28:28 +0530

    Optimize scroll performance and reduce GPU blur lag on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // src/App.jsx
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { ScrollControls, Scroll } from '@react-three/drei'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { ScrollControls, Scroll } from "@react-three/drei";
 
-import SceneContent from './components/SceneContent'
-import HeaderBlock from './components/HeaderBlock'
-import DynamicSponsorsPath from './components/SponsorsPath'
-import PastSponsors from './components/PastSponsors'
-import PartnerWithUs from './components/PartnerWithUs'
+import SceneContent from "./components/SceneContent";
+import HeaderBlock from "./components/HeaderBlock";
+import DynamicSponsorsPath from "./components/SponsorsPath";
+import PastSponsors from "./components/PastSponsors";
+import PartnerWithUs from "./components/PartnerWithUs";
 
-import './style.css'
+import "./style.css";
 
 const App = () => {
   return (
@@ -23,21 +23,22 @@ const App = () => {
         <pointLight position={[-6, 2, 3]} intensity={2} color="#804A8A" />
         <pointLight position={[6, -2, 3]} intensity={2} color="#F59E51" />
 
-        <ScrollControls pages={6.6} damping={0.25}>
+        {/* src/App.jsx */}
+        <ScrollControls pages={6.6} damping={0.08}>
           {/* 3D Scene Cylinder */}
           <SceneContent />
 
           {/* HTML Overlay Stack */}
-          <Scroll html style={{ width: '100vw', pointerEvents: 'none' }}>
+          <Scroll html style={{ width: "100vw", pointerEvents: "none" }}>
             <HeaderBlock />
             <DynamicSponsorsPath />
             <PastSponsors />
-            <PartnerWithUs/>
+            <PartnerWithUs />
           </Scroll>
         </ScrollControls>
       </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
